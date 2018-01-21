9ea2c6d1b50b425bd214c7e1de564795173ac89f
zlodag <11688686+zlodag@users.noreply.github.com>
2018-01-22 01:05:51 +1300

    after add to set

--- src/app/item-detail/item-detail.component.ts
+++ src/app/item-detail/item-detail.component.ts
@@ -15,21 +15,50 @@ export class ItemDetailComponent implements OnInit {
 
   data = {};
 
-  updateOptions : {key: string; label: string;}[] = [
-  	{key: 'introduction', label: 'Introduction'},
-  	{key: 'superiorRelations', label: 'Superior relations'},
-  	{key: 'inferiorRelations', label: 'Inferior relations'},
+  updateOptions : {key: string; label: string; array: boolean}[] = [
+
+  	{key: 'introduction', label: 'Introduction', array: false },
+  	{key: 'structure', label: 'Structure', array: false },
+
+  	{key: 'superiorRelations', label: 'Add superior relation', array: true },
+  	{key: 'inferiorRelations', label: 'Add inferior relation', array: true },
+  	{key: 'anteriorRelations', label: 'Add anterior relation', array: true },
+  	{key: 'posteriorRelations', label: 'Add posterior relation', array: true },
+  	{key: 'medialRelations', label: 'Add medial relation', array: true },
+  	{key: 'lateralRelations', label: 'Add lateral relation', array: true },
+
+  	{key: 'superiorBoundary', label: 'Superior boundary', array: false },
+  	{key: 'inferiorBoundary', label: 'Inferior boundary', array: false },
+  	{key: 'anteriorBoundary', label: 'Anterior boundary', array: false },
+  	{key: 'posteriorBoundary', label: 'Posterior boundary', array: false },
+  	{key: 'medialBoundary', label: 'Medial boundary', array: false },
+  	{key: 'lateralBoundary', label: 'Lateral boundary', array: false },
+
+  	{key: 'contents', label: 'Add content', array: true },
+  	{key: 'articulations', label: 'Add articulation', array: true },
+  	{key: 'attachments', label: 'Add attachment', array: true },
+  	{key: 'specialStructures', label: 'Add special structure', array: true },
+
+  	{key: 'nerveSupply', label: 'Nerve supply', array: false },
+  	{key: 'arterialSupply', label: 'Arterial supply', array: false },
+  	{key: 'venousDrainage', label: 'Venous drainage', array: false },
+  	{key: 'lymphaticDrainage', label: 'Lymphatic drainage', array: false },
+
+  	{key: 'variants', label: 'Add variant', array: true },
+
   ];
 
-  selectedKey = "";
+  selectedOption : {key: string; label: string; array: boolean} = null;
 
   ngOnInit() {
   	this.apiUrl = '/api/items/' + this.route.snapshot.paramMap.get('itemId');
   	for (var i = 0; i < this.updateOptions.length - 1; i++) {
   		let option = this.updateOptions[i];
-  		let value = this.route.snapshot.data.item[option.key];
-  		if (value) {
-			this.data[option.key] = value;
+  		if (!option.array) {
+	  		let value = this.route.snapshot.data.item[option.key];
+	  		if (value) {
+				this.data[option.key] = value;
+	  		}
   		}
   	}
   	// this.introduction = this.route.snapshot.data.item.introduction || "";
@@ -50,13 +79,31 @@ export class ItemDetailComponent implements OnInit {
   // }
 
   update(){
-  	this.data[this.selectedKey] = this.data[this.selectedKey].trim();
+  	this.data[this.selectedOption.key] = this.data[this.selectedOption.key].trim();
   	let updateObject = {};
-  	updateObject[this.selectedKey] = this.data[this.selectedKey];
-  	this.http.put(this.apiUrl, this.data[this.selectedKey].length ? updateObject : {'$unset': updateObject })
-        .subscribe(() => {
-        	this.route.snapshot.data.item[this.selectedKey] = this.data[this.selectedKey];
-        });
+  	updateObject[this.selectedOption.key] = this.data[this.selectedOption.key];
+  	let add = !!this.data[this.selectedOption.key].length;
+  	let array = this.selectedOption.array;
+  	if (array && !add) {
+  		console.error('Trying to delete from array?');
+  		return;
+  	}
+  	let body = {};
+  	body[array ? '$addToSet' : add ? '$set' : '$unset'] = updateObject;
+  	this.http.put(this.apiUrl, body).subscribe((data) => {
+  		if (array) {
+			if (this.route.snapshot.data.item[this.selectedOption.key].indexOf(this.data[this.selectedOption.key]) == -1){
+				this.route.snapshot.data.item[this.selectedOption.key].push(this.data[this.selectedOption.key]);
+			}
+			this.data[this.selectedOption.key] = "";
+		} else if (add) {
+			this.route.snapshot.data.item[this.selectedOption.key] = this.data[this.selectedOption.key];
+		} else {
+  			delete this.route.snapshot.data.item[this.selectedOption.key];
+  		}
+    }, (error) => {
+    	console.error(error);
+    });
   }
 
 }
